db2043d1fcf3a042fba2d9e0eed71bb9a5838379
Ihor Kolotyuk <40246723+kolotyuk@users.noreply.github.com>
2019-06-27 01:15:54 +0300

    Add files via upload

---
+++ src/js/script.js
@@ -0,0 +1,89 @@
+$(document).ready(function() {
+    $(".btn-ricardo").click(function() {
+        $(".btn-ricardo").hide();
+        $(".social-ricardo").fadeIn();
+    });
+});
+$(document).ready(function() {
+    $(".btn-alberto").click(function() {
+        $(".btn-alberto").hide();
+        $(".social-alberto").fadeIn();
+    });
+});
+$(document).ready(function() {
+    $(".btn-thomas").click(function() {
+        $(".btn-thomas").hide();
+        $(".social-thomas").fadeIn();
+    });
+});
+
+
+// scrollTopBtn
+$(document).ready(function() {
+
+    $(window).scroll(function() {
+        if($(this).scrollTop() > 40){
+            $('.topBtn').fadeIn();
+        } else {
+            $('.topBtn').fadeOut();
+        }
+    })
+
+    $('.topBtn').click(function() {
+        $('html, body').animate({scrollTop: 0},500);
+    });
+});
+
+
+// Fixed menu scroll
+$(window).on('scroll', function() {
+    if ($(window).scrollTop()) {
+        $('.header-wrap').addClass('white');
+    } else {
+        $('.header-wrap').removeClass('white');
+    };
+});
+
+
+// Scroll to section
+$('.scrollToSec').click(function () {
+    let getElement = $(this).attr('href');
+    if ($(getElement).length) {
+      let getOffset = $(getElement).offset().top;
+      $('html,body').animate({
+        scrollTop:getOffset
+      },400);
+    }
+    return false;
+});
+
+
+// Counter
+$(document).ready(function(){
+
+	var show = true;
+	var countbox = ".stat";
+	$(window).on("scroll load resize", function(){
+
+		if(!show) return false;
+
+		var w_top = $(window).scrollTop();
+		var e_top = $(countbox).offset().top;
+
+		var w_height = $(window).height();
+		var d_height = $(document).height();
+
+		var e_height = $(countbox).outerHeight();
+
+		if(w_top + 550 >= e_top || w_height + w_top == d_height || e_height + e_top < w_height){
+			$(".counts-block-num").spincrement({
+				thousandSeparator: "",
+				duration: 1200
+			});
+
+			show = false;
+		}
+	});
+});
+
+new WOW().init();
